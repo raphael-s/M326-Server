d6cbda645077659424f2c1762db4944a69a2d604
Adrian Kühni <adrian.kuehni@gmail.com>
2016-11-02 09:24:42 +0100

    added network package

---
+++ src/network/Message.java
@@ -0,0 +1,11 @@
+package network;
+
+/**
+ * Alle Klassen für Nachrichten, welche zwischen dem Bomberman-Server und den Bomberman-Clients
+ * ausgetauscht werden, müssen dieses Interface implementieren.
+ * 
+ * @author Andres Scheidegger
+ *
+ */
+public interface Message {
+}
